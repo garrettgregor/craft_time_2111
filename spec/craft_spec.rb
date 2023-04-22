288a1fc02a02ffe715916b4a50c254b52c9d9eed
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-22 12:32:44 -0600

    Add craft and tests

--- spec/craft_spec.rb
+++ spec/craft_spec.rb
@@ -3,7 +3,11 @@ require "spec_helper"
 RSpec.describe Craft do
   describe "#initialize" do
     it "starts with name and supplies required" do
-      
+      craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+
+      expect(craft).to be_a(Craft)
+      expect(craft.name).to eq("knitting")
+      expect(craft.supplies_required).to eq({:yarn=>20, :scissors=>1, :knitting_needles=>2})
     end
   end
 end
