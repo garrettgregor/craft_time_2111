63060e74b9d61860fd6cb65cbae1a37f5c477fbb
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-22 13:04:49 -0600

    Feature: #attendee_names added to event

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -13,4 +13,38 @@ RSpec.describe Event do
       expect(event.attendees).to eq([person1])
     end
   end
+
+  describe "#attendee names" do
+    it "list names of attendees" do
+      hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+      toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+      sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+      knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+      event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
+
+      expect(event.attendee_names).to eq(["Hector", "Toni"])
+
+      # event.craft_with_most_supplies
+      # #=> "sewing"
+
+      # event.supply_list
+      # #=> ["fabric", "scissors", "thread", "sewing_needles", "yarn", "knitting_needles"]
+    end
+  end
+
+  describe "#craft_with_most_supplies" do
+    it "returns the craft that has the most supplies" do
+      hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+      toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+      sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+      knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+      event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
+
+      event.craft_with_most_supplies
+      #=> "sewing"
+
+      # event.supply_list
+      # #=> ["fabric", "scissors", "thread", "sewing_needles", "yarn", "knitting_needles"]
+    end
+  end
 end
